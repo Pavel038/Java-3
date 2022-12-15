64b42d61853e2882b7cbc95887918d5834401564
Pavel <zenzin92@mail.ru>
2022-12-15 21:56:20 +0700

    first commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int x = 1000; // цена одного билета
+        int y = 20; // количесво рублей для 1 мили
+        int mili = x / y;
+
+        System.out.println("Начислено бонусов: " + mili);
+    }
+}
+

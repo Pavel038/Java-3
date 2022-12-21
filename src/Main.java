1076616f7b2a81882a9fa812bad6354872b78d68
Pavel038 <98365181+Pavel038@users.noreply.github.com>
2022-12-21 20:40:47 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        int ticket_price = 1000; // цена одного билета
-        int mile_price = 20; // количесво рублей для 1 мили
-        int mili = ticket_price / mile_price;
+        int ticketPrice = 1000; // цена одного билета
+        int milePrice = 20; // количесво рублей для 1 мили
+        int mili = ticketPrice / milePrice;
 
         System.out.println("Начислено бонусов: " + mili);
     }
